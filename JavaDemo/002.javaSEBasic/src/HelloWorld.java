d716b09d1a10b895df2d8284101f04c9b3c79449
Thales <1134941536@qq.com>
2021-12-13 16:00:52 +0800

    feat:基础&入门

---
+++ JavaDemo/002.javaSEBasic/src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    // 单行注释
+
+    /*
+    多
+    行
+    注
+    释
+     */
+
+    /**
+     * 文档注释
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        System.out.println("你好啊，小爪！");
+    }
+}
